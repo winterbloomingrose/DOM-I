4425f7cb1e359cee1aed26ff8f89780a654c8ad8
Cipta <42090335+winterbloomingrose@users.noreply.github.com>
2021-05-06 01:12:37 -0400

    Setting up JS File

--- js/index.js
+++ js/index.js
@@ -40,3 +40,26 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// line 9 //
+let titledom = document.getElementByClass("title");
+titledom.textContent = 'DOM';
+
+// lines 20-25 // 
+
+// line 28 //
+const logoimg = document.getElementById ('logo-img');
+logoimg.src = 'img/logo.png';
+// line 33-34// 
+// line 36 // 
+// line 43-46 // 
+// line 49 - 52// 
+// line 56-57 // 
+// line 61- 64 // 
+// line 67-70 //  
+// line 73-76// 
+// line 84// 
+// line 86-87// 
+// line 89 // 
+// line 91 // 
+// line 96 //  
